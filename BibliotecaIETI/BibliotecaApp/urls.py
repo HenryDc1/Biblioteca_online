c2aea381c9442537251ca9e576727fffc8976df9
Javier Bernabeu <javierbernabeu3d@gmail.com>
2024-04-26 16:36:09 +0200

    finished 1/2 change user image

--- BibliotecaIETI/BibliotecaApp/urls.py
+++ BibliotecaIETI/BibliotecaApp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('logout', views.logout_user, name='logout'),
     path('canviar_contrasenya', views.canviar_contrasenya, name='canviar_contrasenya'),
+
+    path('dashboard', views.dashboard, name='dashboard'),
     path('dashboard/usuari', views.usuari, name='usuari'),   
 
     # Reset password 
