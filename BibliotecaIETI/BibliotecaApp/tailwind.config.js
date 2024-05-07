53830e7d502592d5f45b6d0b64063b7d184bec82
Javier Bernabeu <javierbernabeu3d@gmail.com>
2024-05-07 19:10:12 +0200

    responsive a mitad

--- BibliotecaIETI/BibliotecaApp/tailwind.config.js
+++ BibliotecaIETI/BibliotecaApp/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/myapp/*.{html,js}", "./templates/myapp/**/*.{html,js}", "./templates/myapp/**/**/*.{html,js}", "./templates/myapp/registration/*.{html,js}", "./templates/myapp/dashboard/canviar_contrasenya.html", ],
+  content: ["./node_modules/flowbite/**/*.js","./templates/myapp/*.{html,js}", "./templates/myapp/**/*.{html,js}", "./templates/myapp/**/**/*.{html,js}", "./templates/myapp/registration/*.{html,js}", "./templates/myapp/dashboard/canviar_contrasenya.html", ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('flowbite/plugin')
+]}
 
